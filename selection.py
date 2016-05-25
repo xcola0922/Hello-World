deddf0ee915071441f1ce2b9a3d8b48640b8cc87
xcola <xcola0922@gmail.com>
2016-05-25 16:57:20 +0800

    20160525

---
+++ selection.py
@@ -0,0 +1,8 @@
+a = input("input number")
+if a > "5" :
+   print("Oh you are so big")
+else:
+   print("OH you are so small")
+   if a == "5":
+       print("hey my brother")
+   
---
+++ selection2.py
@@ -0,0 +1,14 @@
+a = input("input number : ")
+if a > "5":
+   print("Oh you are so big")
+if a < "5":
+   print("OH you are so small")
+if a == "5":
+    print("hey you are my brother")
+else:
+    print("you kidding me?")
+
+    
+    
+ 
+   
