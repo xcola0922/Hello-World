fa859ba60c9eba6059bc2fc18272839ab3d34109
xcola <xcola0922@gmail.com>
2016-05-27 14:38:00 +0800

    20160527

---
+++ loop7.py
@@ -0,0 +1,9 @@
+i = 1
+j = 1
+while i <= 9:
+    while j <= 9:
+        print(i * j, end=" ")
+        j += 1
+    i += 1
+    j = 1
+    print()
---
+++ loop8.py
@@ -0,0 +1,7 @@
+x = 0
+i = eval(input("Input Number："))
+while True:
+    print (x)
+    x += 1
+    if x > i :
+        break
