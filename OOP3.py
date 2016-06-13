11bfa47a68edc69c7d64290d42600dbead8f168a
xcola <xcola0922@gmail.com>
2016-06-13 16:02:13 +0800

    20160527

---
+++ OOP3.py
@@ -0,0 +1,21 @@
+class suite1:
+    def __init__(self, mic, coke, French_Fries):
+        self.mic = mic
+        self.coke = coke
+        self.French_Fries= French_Fries
+    def set_set(self):
+        return (self.mic + self.coke + self.French_Fries) * 0.8
+class bigmore(suite1):
+    def __init__(self,big1 ):
+        self.big1=big1
+    def big_over(self):
+        return self.big1 + s.set_set()
+a = eval(input("input mic price : "))
+b = eval(input("input coke price : "))
+c = eval(input("input Frech Fries price : "))
+s = suite1(a, b, c)
+s.set_set()
+print ("大麥克套餐：", s.set_set())
+m = bigmore( eval(input("input bigmore price : ")))
+m.big_over()
+print ("薯條可樂加大：", m.big_over())
